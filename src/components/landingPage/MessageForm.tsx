dfccbfd048b61c08ec3ef2be1a808bff1cbe5e00
thelonehegelian <thelonehegelian@gmail.com>
2023-09-11 23:50:01 +0100

    update message form with more inputs and button + add api handler

--- src/components/landingPage/MessageForm.tsx
+++ src/components/landingPage/MessageForm.tsx
@@ -10,19 +10,60 @@ interface HappyNewYearNFT {
   senderAddress: string;
 }
 export default function MessageForm() {
-  const [userRequest, setUserRequest] = useState<HappyNewYearNFT>()
+ const initialUserRequest: HappyNewYearNFT = {
+    nftTitle: '',
+    message: '',
+    owner: '',
+    unlockDate: '',
+    senderName: '',
+    senderAddress: '',
+  };
+  const [userRequest, setUserRequest] = useState<HappyNewYearNFT>(initialUserRequest)
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserRequest((prevUserRequest) => ({ ...prevUserRequest, [name]: value }));
+
+  }
+
+
+const handleCreateCapsule = async () => {
+  // TODO add validation
+  try {
+    const response = await fetch('api/hny', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userRequest),
+    });
+
+    if (response.ok) {
+      // Success
+    } else {
+      // Error
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
 
   const [message, setMessage] = useState('')
     return (
-        <div>           
-
+    <>
+    <div  className="artboard artboard-horizontal phone-1">
+          <h1 className='text-sky-200 text-xl'>Create a Time Capsule </h1>
             <input type="text" placeholder="Title" className="input input-bordered w-full max-w-xs" />
             <input type="text" placeholder="Message" className="input input-bordered w-full max-w-xs" />
             <input type="text" placeholder="Receiver Address" className="input input-bordered w-full max-w-xs" />
             {/* TODO Add this option later */}
             {/* <input type="text" placeholder="Lock Time/Data" className="input input-bordered w-full max-w-xs" /> */}
             <input type="text" placeholder="Unlock Date" className="input input-bordered w-full max-w-xs" />
-    </div>
+ 
+            <button className="btn btn-primary" onClick={handleCreateCapsule}>Create Capsule</button>
+      </div>
+
+
+    </>
     )
 }
