4c381e95a963639de778cb9b4ea7042fb391d61c
thelonehegelian <thelonehegelian@gmail.com>
2023-09-11 04:53:29 +0100

    add details to the form

--- src/components/landingPage/MessageForm.tsx
+++ src/components/landingPage/MessageForm.tsx
@@ -1,12 +1,28 @@
 "use client";
 import {useState} from 'react';
+
+interface HappyNewYearNFT {
+  nftTitle: string;
+  message: string;
+  owner: string;
+  unlockDate: string;
+  senderName: string;
+  senderAddress: string;
+}
 export default function MessageForm() {
-const [message, setMessage] = useState('')
+  const [userRequest, setUserRequest] = useState<HappyNewYearNFT>()
+
+
+  const [message, setMessage] = useState('')
     return (
-        <div>
-            
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+        <div>           
+
+            <input type="text" placeholder="Title" className="input input-bordered w-full max-w-xs" />
+            <input type="text" placeholder="Message" className="input input-bordered w-full max-w-xs" />
+            <input type="text" placeholder="Receiver Address" className="input input-bordered w-full max-w-xs" />
+            {/* TODO Add this option later */}
+            {/* <input type="text" placeholder="Lock Time/Data" className="input input-bordered w-full max-w-xs" /> */}
+            <input type="text" placeholder="Unlock Date" className="input input-bordered w-full max-w-xs" />
     </div>
     )
 }
