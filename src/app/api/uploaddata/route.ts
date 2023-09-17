061f508b5abdd7ca8ea98960cd0746f80be32862
lonehegelian <thelonehegelian@protonmail.com>
2023-09-17 03:20:07 +0100

    add boilerplate for lighthouse sdk

---
+++ src/app/api/uploaddata/route.ts
@@ -0,0 +1,33 @@
+
+import { NextResponse } from 'next/server';
+import lighthouse from '@lighthouse-web3/sdk';
+
+// interface LighthouseResponse { }
+
+const api_key = process.env.API_KEY || '';
+const uploadFile = async (filePath: string, api_key: string | undefined) => {
+  const path = filePath; // Provide the path to the file
+  const apiKey = api_key
+  // Generate the API key from https://files.lighthouse.storage/ 
+  //or using CLI (lighthouse-web3 api-key --new)
+
+  // Both files and folders are supported by the upload function
+  const response = await lighthouse.upload(path, apiKey);
+
+  console.log(response);
+  console.log("Visit at: https://gateway.lighthouse.storage/ipfs/" + response.data.Hash);
+
+  const responseObj = { responseFromLightHouse: response, dataUrl: `https://gateway.lighthouse.storage/ipfs/${response.data.Hash}` };
+  return responseObj;
+}
+
+
+export async function POST(request: Request) {
+  console.log(request);
+
+  // NOTE: file path would be local to the project directory for our demo
+  const responseFromLightHouse = uploadFile("path/to/file", process.env.API_KEY);
+  // TODO send response that the data was uploaded to Filecoin
+  // Note this will change the button to "Mint NFT"
+  return new Response('Hello world!');
+}
