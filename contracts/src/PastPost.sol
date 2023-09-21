5af6c42fbd96666f3620ac67f7284aab732998d4
Ekaterina Ryazantseva <sibkatya@gmail.com>
2023-09-21 16:30:03 +0300

    add tableland

--- contracts/src/PastPost.sol
+++ contracts/src/PastPost.sol
@@ -1,41 +1,78 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
+pragma solidity 0.8.12;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./NFTContract.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@tableland/evm/contracts/utils/TablelandDeployments.sol";
+import "@tableland/evm/contracts/utils/SQLHelpers.sol";
 
-contract PastPost is Ownable {
+contract PastPost is Ownable, ERC721Holder {
+    uint256 private tableId;
+    string public constant TABLE_PREFIX = "pastpost"; 
 
-    string public uri;
+    string public uri = "";
 
     struct Capsule {
         address owner;
         address nftContract;
     }
 
-    mapping(address => address[]) userNftContracts;
+    mapping(address => address[]) public userNftContracts;
     Capsule[] public timeCapsules;
     
+    event TableCreated(uint256 tableId, string tableName);
     event TimeCapsuleCreated(address owner, address nft);
 
-    constructor(string memory _uri) {
-        uri = _uri;
+    constructor() {
+        tableId = TablelandDeployments.get().create(
+            address(this),
+            SQLHelpers.toCreateFromSchema(
+                "id integer primary key,"
+                "capsule text,"
+                "cid text",
+                TABLE_PREFIX
+            )
+        );
     }
 
-    function createTimeCapsule(address receiver) public {
+    function tableName() external view returns (string memory) {
+        return SQLHelpers.toNameFromId(TABLE_PREFIX, tableId);
+    }
+
+    function createTimeCapsule(address receiver, string[] calldata _cids) public {
         NFTContract nft = new NFTContract();
         userNftContracts[msg.sender].push(address(nft));
         Capsule memory newCapsule = Capsule({owner: msg.sender, nftContract: address(nft)});
         timeCapsules.push(newCapsule);
         nft.safeMint(msg.sender, uri);
-        if (receiver != address(0)) {
+        if (receiver != msg.sender) {
             nft.safeMint(receiver, uri);
         }
         nft.transferOwnership(msg.sender);
+
+        string[] memory values = new string[](_cids.length);
+        for (uint i = 0; i < _cids.length; i++) {
+            values[i] = string(abi.encodePacked("'", address(nft), " ", _cids[i], "'"));
+        }
+
+        TablelandDeployments.get().mutate(
+            address(this),
+            tableId,
+            SQLHelpers.toBatchInsert(
+                TABLE_PREFIX,
+                tableId,
+                "capsule, cid",
+                values
+            )
+        );
         emit TimeCapsuleCreated(msg.sender, address(nft));
     }
 
     function setURI(string memory _uri) public onlyOwner {
         uri = _uri;
     }
+
+
 }
