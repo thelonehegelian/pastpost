809bca441949ec8fc25749edf6ba7cd3ce56d7b1
Jayanth <jayanth.sms.in@gmail.com>
2023-09-24 01:34:43 +0530

    adding confetti

---
+++ src/components/Friendsdata.js
@@ -0,0 +1,81 @@
+import video from '../../public/1cmp.svg';
+import text from '../../public/2cmp.svg';
+import image from '../../public/3cmp.svg';
+const big = [
+  {
+    image: video,
+    Name: 'Alice',
+    wallet_id: '0xAbCdEf0123456789',
+  },
+  {
+    image: text,
+    Name: 'Bob',
+    wallet_id: '0x1234567890abcdef',
+  },
+  {
+    image: image,
+    Name: 'Charlie',
+    wallet_id: '0x9876543210fedcba',
+  },
+  {
+    image: video,
+    Name: 'David',
+    wallet_id: '0xABCDEF0123456789',
+  },
+  {
+    image: text,
+    Name: 'Eve',
+    wallet_id: '0xFEDCBA0987654321',
+  },
+  {
+    image: video,
+    Name: 'Frank',
+    wallet_id: '0x0123456789abcdef',
+  },
+  {
+    image: text,
+    Name: 'Grace',
+    wallet_id: '0x13579BDFECA86420',
+  },
+  {
+    image: image,
+    Name: 'Hank',
+    wallet_id: '0xC0FFEEDEADBEEF42',
+  },
+  {
+    image: text,
+    Name: 'Ivy',
+    wallet_id: '0x9876123450FEDCBA',
+  },
+  {
+    image: image,
+    Name: 'Jack',
+    wallet_id: '0x12345ABCDEF67890',
+  },
+  //   {
+  //     image: video,
+  //     Name: 'Karen',
+  //     wallet_id: '0xDEADBEEF98765432',
+  //   },
+  //   {
+  //     image: text,
+  //     Name: 'Liam',
+  //     wallet_id: '0x98765FEDCBA43210',
+  //   },
+  //   {
+  //     image: image,
+  //     Name: 'Megan',
+  //     wallet_id: '0x543210CBAFED9876',
+  //   },
+  //   {
+  //     image: video,
+  //     Name: 'Nina',
+  //     wallet_id: '0x7890ABCDEF012345',
+  //   },
+  //   {
+  //     image: text,
+  //     Name: 'Oliver',
+  //     wallet_id: '0x4567890ABCDEF012',
+  //   },
+];
+export default big;
