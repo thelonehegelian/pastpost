dcff3794d60f6668e4c6c3834a2855d1eaccda65
lonehegelian <thelonehegelian@protonmail.com>
2023-09-25 19:37:30 +0100

    update landingpage component

--- src/app/landingpage/page.tsx
+++ src/app/landingpage/page.tsx
@@ -1,15 +1,10 @@
+import React from 'react';
 import MessageForm from '../../components/landingPage/MessageForm';
 
-interface MessageProps {
-  text: string;
-}
-// @dev why are the messageProps needed?
-const Message: React.FC<MessageProps> = ({ text }) => {
+export default function Message(): JSX.Element {
   return (
     <div className="flex justify-center items-center h-screen">
       <MessageForm />
     </div>
   );
-};
-
-export default Message;
+}
