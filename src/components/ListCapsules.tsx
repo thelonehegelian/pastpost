913f6999184395ad92695d547c731fb719d50615
lonehegelian <thelonehegelian@protonmail.com>
2023-09-26 02:25:00 +0100

    update styles to shorten cid and add links to cid to open capsules

--- src/components/ListCapsules.tsx
+++ src/components/ListCapsules.tsx
@@ -1,20 +1,53 @@
 import React from 'react';
 import small from './dummydata.js';
 import Image from 'next/image';
+import Link from 'next/link';
+interface ListCapsulesProps {
+  received: boolean;
+  handleAcceptance: () => void;
+}
 
-const ListCapsules = () => {
+const ListCapsules = ({ received, handleAcceptance }: ListCapsulesProps) => {
   return (
     <>
-      {small.map((item, index) => (
-        <div key={index} className="flex py-2 justify-around">
-          <Image src={item.image} className="h-20 w-20 mx-4 " alt="image" />
-          <div>
-            <h3 className="font-bold text-xl py-2">{item.wallet_id}</h3>
-            <p> {item.text}</p>
+      {small.map((item, index) =>
+        received ? (
+          <div key={index} className="flex py-2 justify-around">
+            <Image src={item.image} className="h-20 w-20 mx-4 " alt="image" />
+            <div>
+              <h3 className="font-bold text-xl py-2">
+                <Link href={`https://files.lighthouse.storage/viewFile/${item.cid}`}>
+                  `${item.cid.substring(0, 8)}...`
+                </Link>
+              </h3>
+              <p> {item.text}</p>
+            </div>
+            <button className="bg-[#D8F9E4] h-full p-2 rounded-2xl">
+              {item.button.options[0]}
+            </button>
+
+            <button className="bg-[#D8F9E4] h-full p-2 rounded-2xl" onClick={handleAcceptance}>
+              Accept
+            </button>
           </div>
-          <button className="bg-[#D8F9E4] h-full p-2 rounded-2xl">{item.button.options[0]}</button>
-        </div>
-      ))}
+        ) : (
+          // SENT
+          <div key={index} className="flex py-2 justify-around">
+            <Image src={item.image} className="h-20 w-20 mx-4 " alt="image" />
+            <div>
+              <h3 className="font-bold text-xl py-2">
+                <Link href={`https://files.lighthouse.storage/viewFile/${item.cid}`}>
+                  `${item.cid.substring(0, 8)}...`
+                </Link>
+              </h3>
+              <p> {item.text}</p>
+            </div>
+            <button className="bg-[#D8F9E4] h-full p-2 rounded-2xl">
+              {item.button.options[0]}
+            </button>
+          </div>
+        ),
+      )}
     </>
   );
 };
